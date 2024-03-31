7ea67d4aa66b70d8512d4f4b90ae86bd5e895f46
Mahemo <mario.hemo2004@gmail.com>
2024-03-31 09:02:40 -0600

    solucion de unos problemas con los saltos de linea

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ def extract_text_from_pdf(pdf_file: str) -> list[str]:
 
             cleaned_content = re.sub(r'[^\w\s.,:;%]', '', content)
 
-            text = cleaned_content.split('  ')
+            text_lines = re.split(r'(?<=[.,:;])\s+|\s{2,}', cleaned_content)
             
-            cleaned_text = [re.sub(r'[.,:;]', '', line.strip()) for line in text if line.strip()]
+            cleaned_text = [re.sub(r'[.,:;]', '', line.strip()) for line in text_lines if line.strip()]
 
             pdf_text.extend(cleaned_text)
 
@@ -30,8 +30,8 @@ def count_repetitions(pdf_text: list[str], dict_phrases: list[str]) -> dict[str,
     return repeated_phrase
 
 def main() -> None:
-    pdf_text: list[str] = extract_text_from_pdf('Sample 2.pdf')
-    dict_phrases: list[str] = ['documento', 'Texto', 'Sample PDF 1', 'Texto de relleno']
+    pdf_text: list[str] = extract_text_from_pdf('Sample.pdf')
+    dict_phrases: list[str] = extract_text_from_pdf('Sample.pdf')
 
     phrases_repetitions: dict[str, int] = count_repetitions(pdf_text, dict_phrases)
 
