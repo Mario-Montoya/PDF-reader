397ff2e083c9940e633b82bac01eac4e0179f67a
Mahemo <mario.hemo2004@gmail.com>
2024-03-31 08:01:44 -0600

    count_repetitons funciona pero cuenta de mas unas cosas

--- main.py
+++ main.py
@@ -13,15 +13,27 @@ def extract_text_from_pdf(pdf_file: str) -> list[str]:
             
             cleaned_text = [line.strip() for line in text if line.strip()]
 
-            pdf_text.append(cleaned_text)
+            pdf_text.extend(cleaned_text)
 
         return pdf_text
     
-def main() -> None:
-    text_pdf = extract_text_from_pdf('Sample.pdf')
+def count_repetitions(pdf_text: list[str], dict_phrases: list[str]) -> dict[str, int]:
+    repeated_phrase = {phrase: 0 for phrase in dict_phrases}
+    
+    for phrase in dict_phrases:
+        for text in pdf_text:
+            repeated_phrase[phrase] += text.lower().count(phrase.lower())
 
-    for page in text_pdf:
-        print(page)
+    return repeated_phrase
+
+def main() -> None:
+    pdf_text: list[str] = extract_text_from_pdf('Sample.pdf')
+    dict_phrases: list[str] = ['documento', 'Texto', 'Sample PDF 1']
+
+    phrases_repetitions: dict[str, int] = count_repetitions(pdf_text, dict_phrases)
+
+    for phrase, repetiton in phrases_repetitions.items():
+        print(f'"{phrase}" aparece {repetiton}')
     
 if __name__ == '__main__':
     main()
